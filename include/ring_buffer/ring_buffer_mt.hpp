6a7749d4a53d3345482b48c7d35def852245fc33
Hank Meng <ymenghank@gmail.com>
2022-01-17 21:37:42 +0800

    fix typo; add comment

--- include/ring_buffer/ring_buffer_mt.hpp
+++ include/ring_buffer/ring_buffer_mt.hpp
@@ -8,16 +8,21 @@
 
 namespace ring_buffer
 {
+    /// \class ring_buffer_mt
+    /// \brief Multi-writer ring buffer.
     struct ring_buffer_mt
     {
-        unsigned char *buf;
-        std::size_t capacity;
-        std::atomic<std::size_t> *head_commited;
+        unsigned char *buf;                       //< Point to a block of memory
+        std::size_t capacity;                     //< Size of the block of memory
+        std::atomic<std::size_t> *head_committed; //< Initially, head_committed == head_pending == 0
         std::atomic<std::size_t> *head_pending;
     };
 
     /// \brief Write \a len bytes from \a src to ring buffer \a ring.
-    /// \return len
+    /// \param src data to write
+    /// \param len size of the data; must not exceed the capacity of the \a ring.
+    /// \param ring the ring buffer to write data to
+    /// \return \a len
     std::size_t write(const void *src, std::size_t len, ring_buffer_mt &ring)
     {
         auto head_old = ring.head_pending->fetch_add(len);
@@ -34,17 +39,21 @@ namespace ring_buffer
             std::memcpy(ring.buf, (const unsigned char *)src + len - i_end, i_end);
         }
         for (auto ho = head_old;
-             !std::atomic_compare_exchange_weak(ring.head_commited, &ho, head_new);
+             !std::atomic_compare_exchange_weak(ring.head_committed, &ho, head_new);
              ho = head_old)
             ;
         return len;
     }
 
     /// \brief Read at most \a len bytes into \a dst from ring buffer \a ring.
-    /// \return The actual number of bytes read
+    /// \param dst buffer to receive data
+    /// \param len maximum bytes to read
+    /// \param ring the ring buffer to read data from
+    /// \param tail position of the individual consumer
+    /// \return the actual number of bytes read
     std::size_t read(void *dst, std::size_t len, ring_buffer_mt &ring, std::size_t &tail)
     {
-        auto head = ring.head_commited->load();
+        auto head = ring.head_committed->load();
         if (tail >= head)
         {
             return 0;
